6c63f28b7704a492e6e7590606223d216cf6f4f2
ashik <2021pgcaca036@nitjsr.ac.in>
2026-03-24 11:21:03 +0530

    start later

--- src/main/java/com/application/bookMyShow/controllers/LanguageController.java
+++ src/main/java/com/application/bookMyShow/controllers/LanguageController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("languages")
+@RequestMapping("/languages")
 public class LanguageController {
 
     @Autowired
